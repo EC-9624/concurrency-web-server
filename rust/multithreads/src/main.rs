53f495d8389e7b6a62ca631572d0f61d0989f86a
eakudompong.chanoknan <ceguantz96@outlook.com>
2024-06-11 15:01:11 +0900

    impl rust

--- rust/multithreads/src/main.rs
+++ rust/multithreads/src/main.rs
@@ -1,3 +1,30 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+extern crate futures;
+use futures::executor::ThreadPoolBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // bind listener
+
+    let mut pool_builder = ThreadPoolBuilder::new();
+    pool_builder.pool_size(100);
+    let pool = pool_builder.create().expect("couldn't create threadpool");
+
+    // Listen for an incoming connection.
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // spawning each connection in a new thread asynchronously
+        pool.spawn_ok(async {
+            handle_connection(stream).await;
+        });
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+
+    stream.write(response.as_bytes()).unwrap(); // write response
+    stream.flush().unwrap();
 }
